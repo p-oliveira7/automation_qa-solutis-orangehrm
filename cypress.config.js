8a4f7df903e9564400700ba5e899c15dead677c7
Paulo Oliveira <111186037+p-oliveira7@users.noreply.github.com>
2023-10-19 12:29:32 -0300

    feat: Adiciona testes de login e criação de novo funcionário

--- cypress.config.js
+++ cypress.config.js
@@ -2,9 +2,11 @@ const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
   e2e: {
-    baseUrl: "https://opensource-demo.orangehrmlive.com/web/index.php",
+    baseUrl: "https://opensource-demo.orangehrmlive.com/web/index.php?language=en",
     viewportHeight:720,
     viewportWidth:1280,
+    defaultCommandTimeout: 6000,
+    requestTimeout:6000,
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
--- cypress/e2e/addEmployee.cy.js
+++ cypress/e2e/addEmployee.cy.js
@@ -1,10 +1,22 @@
+import {fakerPT_BR} from '@faker-js/faker'
+
 const username = Cypress.env("ORANGE_USERNAME");
 const password = Cypress.env("ORANGE_PASSWORD");
+let person;
+
 
 
 describe('Acessar o menu e realizar o cadastro de um novo funcionário', () => {
 
   beforeEach(() => {
+    person = {
+      firstName: fakerPT_BR.person.firstName(),
+      middleName: fakerPT_BR.person.middleName(),
+      lastName: fakerPT_BR.person.lastName(),
+      id: fakerPT_BR.random.numeric(7),
+      image: 'cypress/images/caneta.png'
+    };
+
     cy.login(username, password);
   });
   it('Deve acessar a área PIM', () => {
@@ -30,7 +42,33 @@ describe('Acessar o menu e realizar o cadastro de um novo funcionário', () => {
     .should('have.text', 'Add Employee')
   });
 
-  it('Deve Adicionar um funcionario ', () => {
+  it('Deve adicionar um funcionário e validar mensagem de sucesso', () => {
+    // Preenche os campos de nome e id do funcionário
+    cy.fillNewEmployeeForm(person);
+
+    // Verifica mensagem de sucesso
+    cy.get('.oxd-toast')
+    .should('be.visible')
+    .contains('Successfully Saved');
+  });
+
+  it('Deve adicionar um funcionário e validar as informações', () => {
+    // Preenche os campos de nome e id do funcionário
+    cy.fillNewEmployeeForm(person);
+
+    // Verifica se os dados batem com os exibidos nos detalhes do funcionário
+    cy.validateEmployeeDetails(person);
+  });
+
+  it('Deve adiciona um funcionario e validar os detalhes pela lista de funcionários', () => {
     
+    // Preenche os campos de nome e id do funcionário
+    cy.fillNewEmployeeForm(person);
+
+    // Pesquisa pelo funcionario passando nome completo e Id
+    cy.searchEmployee(person);
+
+    // Verifica se os dados batem com os exibidos nos detalhes do funcionário
+    cy.validateEmployeeDetails(person);
   });
 })
